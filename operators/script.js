59e26be21840d172f874405ce68c2ef7b90ed550
AZE <example@gmail.com>
2023-10-04 00:08:12 +0500

    first commit

---
+++ operators/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+console.log('arr' + ' - object');
+console.log(4 + '5');
+
+let incr = 10,
+    dcr = 10;
+
+// ++incr;
+// --dcr
+
+console.log(++incr);
+console.log(--dcr);
+
+console.log(5%2);
+
+console.log(2 + 2 * 2 !== '6');
+
+const isChecked = false,
+      isClose = false;
+
+console.log(isChecked || !isClose);
---
+++ simple using js with user/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// alert('Hello!');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = +prompt('Are you 18?', '18');
+// console.log(answer + 5);
+
+const answers = [];
+
+answers[0] = prompt('What is your name?', '');
+answers[1] = prompt('What is your surname?', '');
+answers[2] = prompt('How old r u?', '');
+
+console.log(typeof(answers));
+console.log(typeof(null));
