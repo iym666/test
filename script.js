23d7fa03d87429e248682c82194e843e4d50f1ad
AZE <example@gmail.com>
2023-10-04 14:35:00 +0500

    recording two answers to object with name 'movies'

--- script.js
+++ script.js
@@ -15,3 +15,7 @@ let firstQuestion = prompt('One of the last watched movie?', ''),
     thirdQuestion = prompt('One of the last watched movie?', ''),
     fourthQuestion = prompt('How much would you rate the movie?', '');
 
+personalMovieDB.movies[firstQuestion] = secondQuestion;
+personalMovieDB.movies[thirdQuestion] = fourthQuestion;
+    
+console.log(personalMovieDB);
