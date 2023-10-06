f3a8338651a99cd76e3043448a5ea7c92cc065ce
AZE <example@gmail.com>
2023-10-07 00:19:56 +0500

    created second variation of while loop

--- script.js
+++ script.js
@@ -35,5 +35,33 @@ for (let i = 0; i < 2; i++) {
   }
 
 }
+
+// second variation of while loop
+let count = 0;
+
+while (count < 2) {
+  let firstQuestion = prompt('One of the last watched movie?', ''),
+      secondQuestion = prompt('How much would you rate the movie?', '');
+
+  if (firstQuestion != null && secondQuestion != null && firstQuestion != '' && secondQuestion != '' && firstQuestion.length <= 50 && secondQuestion.length <= 50) {
+    personalMovieDB.movies[firstQuestion] = secondQuestion;
+    console.log('done!')
+  } else {
+    console.log('error');
+    i--;
+  }
+
+  if (personalMovieDB.count < 10) {
+    console.log('You watched pretty few films!');
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('You are classic watcher!');
+  } else if (personalMovieDB.count > 30) {
+    console.log('You are movie geek!');
+  } else {
+    console.log('Error');
+  }
+
+  count++;
+}
     
 console.log(personalMovieDB);
