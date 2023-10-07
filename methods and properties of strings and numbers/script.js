0f636891ec169f8b389eeac06b9e7131e4b34fae
AZE <example@gmail.com>
2023-10-07 16:51:26 +0500

    Created new js file with practice of methods and properties

---
+++ methods and properties of strings and numbers/script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const str = 'teSt';
+const arr = [1, 2, 4];
+
+// console.log(str.length);
+// console.log(arr.length);
+// console.log(str[2]);
+
+//console.log(str[2] = 'd');
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str);
+
+const fruit = 'Some fruit';
+
+console.log(fruit.indexOf('fruit'));
+
+const logg = 'Hello World!';
+
+console.log(logg.slice(6, 11));
+
+console.log(logg.substring(6, 11));
+
+console.log(logg.substr(6, 5));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = '12.2px';
+console.log(parseInt(test));
+console.log(parseFloat(test));
