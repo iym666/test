92a5491e12d67ff54401eed00d8c35ffaa5cf296
AZE <example@gmail.com>
2023-10-10 01:08:22 +0500

    created new function detectPersonalLevel and put there if statement to call it whenever it need to be

--- script.js
+++ script.js
@@ -22,24 +22,24 @@ let personalMovieDB = {
 
 
 //first variation of for loop
-
-
-  function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-      let firstQuestion = prompt('One of the last watched movie?', ''),
-          secondQuestion = prompt('How much would you rate the movie?', '');
-    
-      if (firstQuestion != null && secondQuestion != null && firstQuestion != '' && secondQuestion != '' && firstQuestion.length <= 50 && secondQuestion.length <= 50) {
-        personalMovieDB.movies[firstQuestion] = secondQuestion;
-        console.log('done!')
-      } else {
-        console.log('error');
-        i--;
-      }
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    let firstQuestion = prompt('One of the last watched movie?', ''),
+        secondQuestion = prompt('How much would you rate the movie?', '');
+  
+    if (firstQuestion != null && secondQuestion != null && firstQuestion != '' && secondQuestion != '' && firstQuestion.length <= 50 && secondQuestion.length <= 50) {
+      personalMovieDB.movies[firstQuestion] = secondQuestion;
+      console.log('done!')
+    } else {
+      console.log('error');
+      i--;
+    }
   }
+}
 
-  rememberMyFilms();
+rememberMyFilms();
 
+function detectPersonalLevel() {
   if (personalMovieDB.count < 10) {
     console.log('You watched pretty few films!');
   } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
@@ -49,8 +49,9 @@ let personalMovieDB = {
   } else {
     console.log('Error');
   }
-
 }
+
+detectPersonalLevel();
 
 // second variation of while loop
 // let count = 0;
