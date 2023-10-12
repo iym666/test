261bcd9e5825a6c1aa53c436b306856130a9b0ea
AZE <example@gmail.com>
2023-10-13 01:10:45 +0500

    practicing with common interview tasks

---
+++ tasks/script.js
@@ -0,0 +1,46 @@
+// 1st task
+let x = 5;
+alert(++x);
+
+// 2nd task
+let example = [] + false - null + true;
+console.log(example);
+
+// 3rd task
+// let y = 1;
+// let x = y = 2;
+// alert(x);
+
+// 4th task
+let example4 = [] + 1 + 2;
+console.log(example4);
+
+// 5th task
+alert('1'[0]);
+
+// 6th task 
+// AND is stumbles on false!!!!!!!!
+// OR is stumbles on true!!!!!!!!!
+let operators = 2 && 1 && null && 0 && undefined;
+console.log(operators);
+
+// 7th task
+console.log(!!(1 && 2) === (1 && 4));
+
+// 8th task
+alert(null || 2 && 3 || 4);
+
+// 9th task
+const a = [1, 2, 3]; 
+const b = [1, 2, 3];
+console.log(a == b);
+
+// 10th task
+alert(+'Infinity');
+
+// 11th task
+// unicode
+console.log('Ёжик' > 'яблоко');
+
+// 12th task
+console.log(0 || '' || 2 || undefined || true || false);
