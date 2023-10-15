aa056a305dca235afb53a24d2848ddced69f1240
AZE <example@gmail.com>
2023-10-16 00:27:32 +0500

    sorted films from DB and numbered films

--- practice with actions on web page/js/script.js
+++ practice with actions on web page/js/script.js
@@ -28,8 +28,8 @@ const AD = document.querySelector('.promo__adv'),
       advertise = AD.querySelector('.promo__adv-title'),
       images = AD.querySelectorAll('img'),
       contentBG = document.querySelector('.promo__bg'),
-      contentGenre = contentBG.querySelector('.promo__genre');
-
+      contentGenre = contentBG.querySelector('.promo__genre'),
+      movieList = document.querySelector('.promo__interactive-list');
 
 // 1
 images[0].remove()
@@ -50,4 +50,15 @@ contentBG.insertAdjacentHTML('afterbegin', 'DRAMA');
 contentBG.style.backgroundImage = "url('img/bg.jpg')";
 console.log(contentBG);
 
-//4
+//4 && 5
+movieList.innerHTML = '';
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+        <li class="promo__interactive-item">${i + 1} ${film}
+            <div class="delete"></div>
+        </li>
+    `;
+})
