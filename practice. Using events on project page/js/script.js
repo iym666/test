955f17d08e513d6c0af7bfaee5678940b410cbad
AZE <example@gmail.com>
2023-10-17 22:21:13 +0500

    created new event listener dom content loaded to give opportunity for DOM to load every tag in page aftewords to load script

--- practice. Using events on project page/js/script.js
+++ practice. Using events on project page/js/script.js
@@ -1,50 +1,52 @@
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Logan",
-        "League of justice",
-        "La-la land",
-        "Durability",
-        "SCOTT PILGRIM VS..."
-    ]
-};
-
-const AD = document.querySelector('.promo__adv'),
-      advertise = AD.querySelector('.promo__adv-title'),
-      images = AD.querySelectorAll('img'),
-      contentBG = document.querySelector('.promo__bg'),
-      contentGenre = contentBG.querySelector('.promo__genre'),
-      movieList = document.querySelector('.promo__interactive-list');
-
-// 1
-images[0].remove()
-images[1].remove()
-images[2].remove()
-// AD.removeChild(images[0]);
-// AD.removeChild(images[1]);
-// AD.removeChild(images[2]);
-AD.removeChild(advertise);
-
-//2
-
-contentBG.removeChild(contentGenre);
-contentBG.classList.add('promo__genre')
-contentBG.insertAdjacentHTML('afterbegin', 'DRAMA');
-
-//3
-contentBG.style.backgroundImage = "url('img/bg.jpg')";
-console.log(contentBG);
-
-//4 && 5
-movieList.innerHTML = '';
-
-movieDB.movies.sort();
-
-movieDB.movies.forEach((film, i) => {
-    movieList.innerHTML += `
-        <li class="promo__interactive-item">${i + 1} ${film}
-            <div class="delete"></div>
-        </li>
-    `;
-})
+document.addEventListener('DOMContentLoaded', () => {
+    const movieDB = {
+        movies: [
+            "Logan",
+            "League of justice",
+            "La-la land",
+            "Durability",
+            "SCOTT PILGRIM VS..."
+        ]
+    };
+    
+    const AD = document.querySelector('.promo__adv'),
+          advertise = AD.querySelector('.promo__adv-title'),
+          images = AD.querySelectorAll('img'),
+          contentBG = document.querySelector('.promo__bg'),
+          contentGenre = contentBG.querySelector('.promo__genre'),
+          movieList = document.querySelector('.promo__interactive-list');
+    
+    // 1
+    images[0].remove()
+    images[1].remove()
+    images[2].remove()
+    // AD.removeChild(images[0]);
+    // AD.removeChild(images[1]);
+    // AD.removeChild(images[2]);
+    AD.removeChild(advertise);
+    
+    //2
+    
+    contentBG.removeChild(contentGenre);
+    contentBG.classList.add('promo__genre')
+    contentBG.insertAdjacentHTML('afterbegin', 'DRAMA');
+    
+    //3
+    contentBG.style.backgroundImage = "url('img/bg.jpg')";
+    console.log(contentBG);
+    
+    //4 && 5
+    movieList.innerHTML = '';
+    
+    movieDB.movies.sort();
+    
+    movieDB.movies.forEach((film, i) => {
+        movieList.innerHTML += `
+            <li class="promo__interactive-item">${i + 1} ${film}
+                <div class="delete"></div>
+            </li>
+        `;
+    });
+});
