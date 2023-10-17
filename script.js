de3ee79ebdbd8915bd13c6311aa5ab3a4eeac4e3
AZE <example@gmail.com>
2023-10-18 00:00:57 +0500

    testing async = false to know how it works

--- script.js
+++ script.js
@@ -3,6 +3,14 @@
 const p = document.querySelectorAll('p');
 console.log(p);
 
-const script = document.createElement('script');
-script.src = 'test.js';
-document.body.append(script);
+
+
+function loadScript(src) {
+  const script = document.createElement('script');
+  script.src = src;
+  script.async = false;
+  document.body.append(script);
+}
+
+loadScript('test.js');
+loadScript('some.js');
